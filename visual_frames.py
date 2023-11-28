3d2aee98482e09104475bb27211be0707dd2f3a6
retoc71586 <retoc.spam@gmail.com>
2023-11-28 15:02:12 +0100

    Integrated pose detection

--- visual_frames.py
+++ visual_frames.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
+import cv2 
+from chessboard_pose_estimation import PoseEstimator
 
 np.set_printoptions(suppress=True)
 
@@ -21,6 +22,21 @@ T_robot_april_tag = np.array(
         [0.0, 0.0, 0.0, 1.0],
     ]
 )
+
+# Transforms from robot frame to chessboard
+rotation_matrix, _ = cv2.Rodrigues(-np.pi * np.array([0,0,1]))
+transformation_matrix = np.eye(4)
+transformation_matrix[:3, :3] = rotation_matrix
+transformation_matrix[:3, 3] = [0.702, -0.009, 0.0]
+T_robot_chessboard = np.array(transformation_matrix)
+
+# Transforms from chessboard frame to camera
+T_camera_chessboard = np.array([
+ [ 0.99989423,  0.0035801 ,  0.01409623, -0.89930352],
+ [ 0.00337663, -0.99989016,  0.01443158, -0.37184289],
+ [ 0.01414635, -0.01438246, -0.99979649,  1.86404964],
+ [0.00000000, 0.00000000, 0.00000000, 1.00000000],
+ ])
 
 # Transforms from tag frame to camera frame
 T_camera_tag = np.array(
@@ -50,7 +66,7 @@ T_camera_tag_new = np.array(
 #     ]
 # )
 
-T_robot_camera = np.matmul(T_robot_april_tag, np.linalg.inv(T_camera_tag))
+T_robot_camera = np.matmul(T_robot_chessboard, np.linalg.inv(T_camera_chessboard))
 T_camera_robot = np.linalg.inv(T_robot_camera)
 
 ############################################################################################
@@ -58,44 +74,11 @@ T_camera_robot = np.linalg.inv(T_robot_camera)
 
 def plot_frame(ax, frame, label):
     # x
-    ax.quiver(
-        frame[0, 3],
-        frame[1, 3],
-        frame[2, 3],
-        frame[0, 0],
-        frame[1, 0],
-        frame[2, 0],
-        color="r",
-        length=0.2,
-        normalize=True,
-        zorder=2,
-    )
+    ax.quiver(frame[0, 3],frame[1, 3],frame[2, 3],frame[0, 0],frame[1, 0],frame[2, 0],color="r",length=0.2,normalize=True,zorder=2,)
     # y
-    ax.quiver(
-        frame[0, 3],
-        frame[1, 3],
-        frame[2, 3],
-        frame[0, 1],
-        frame[1, 1],
-        frame[2, 1],
-        color="g",
-        length=0.2,
-        normalize=True,
-        zorder=2,
-    )
+    ax.quiver(frame[0, 3],frame[1, 3],frame[2, 3],frame[0, 1],frame[1, 1],frame[2, 1],color="g",length=0.2,normalize=True,zorder=2,)
     # z
-    ax.quiver(
-        frame[0, 3],
-        frame[1, 3],
-        frame[2, 3],
-        frame[0, 2],
-        frame[1, 2],
-        frame[2, 2],
-        color="b",
-        length=0.2,
-        normalize=True,
-        zorder=2,
-    )
+    ax.quiver(frame[0, 3],frame[1, 3],frame[2, 3],frame[0, 2],frame[1, 2],frame[2, 2],color="b",length=0.2,normalize=True,zorder=2,)
 
     ax.text(frame[0, 3], frame[1, 3], frame[2, 3], label, fontsize=12)
 
@@ -127,29 +110,33 @@ def plot_rectangle(ax):
             Poly3DCollection(surface, color="burlywood", alpha=1, zorder=1, shade=False)
         )
 
+if __name__ == "__main__":
+    # Get chessboard pose in camera frame
+    estimator = PoseEstimator()
+    rvecs, tvecs = estimator.estimatePose()
+    transformation_matrix = np.eye(4)
+    transformation_matrix[:3, :3], _ = cv2.Rodrigues(rvecs)
+    transformation_matrix[:3, 3] = tvecs.reshape(3)
+    T_camera_chessboard = np.array(transformation_matrix)
 
-# Define frames (expressed all in tag frame)
-camera = np.eye(4)  # Camera frame expressed in tag frame
-tag = T_camera_tag  # Tag frame expressed in tag frame
-tag_new = T_camera_tag_new
-robot = T_camera_robot
+    # Define frames (expressed all in tag frame)
+    camera = np.linalg.inv(T_camera_chessboard)
+    tag = np.eye(4)
+    robot = np.linalg.inv(T_robot_chessboard)
 
-tag_new = np.eye(4)
-camera = np.linalg.inv(T_camera_tag_new)
-tag = np.linalg.inv(T_camera_tag_new)@T_camera_tag
 
-# Plot the frames
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.set_xlim([-1, 1])
-ax.set_ylim([-1, 1])
-ax.set_zlim([-1, 1])
+    # Plot the frames
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([-1, 1])
 
-plot_frame(ax, camera, "camera")
-plot_frame(ax, tag, "tag")
-plot_frame(ax, tag_new, "tag_new")
-# plot_frame(ax, robot, "robot")
+    plot_frame(ax, camera, "camera")
+    plot_frame(ax, tag, "tag")
+    plot_frame(ax, robot, "robot")
+    # plot_frame(ax, robot, "robot")
 
-ax.legend(labels=["x", "y", "z"], loc="upper right", bbox_to_anchor=(1, 1))
+    ax.legend(labels=["x", "y", "z"], loc="upper right", bbox_to_anchor=(1, 1))
 
-plt.show()
+    plt.show()
