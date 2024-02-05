4f20797de9acba95e0265c07c5418be9a64850e5
retoc71586 <retoc.spam@gmail.com>
2024-02-05 09:28:50 +0100

    updated whole renderer class

--- renderer.py
+++ renderer.py
@@ -111,3 +111,87 @@ class PyRenderer():
 			
 			self._mano_vert.append(mano_vert)
 			self._mano_joint.append(mano_joint)
+
+
+	def _blend(self, im_real, im_render):
+		"""Blends the real and rendered images.
+
+		Args:
+		  	im_real: A uint8 numpy array of shape [H, W, 3] containing the real image.
+		  	im_render: A uint8 numpy array of shape [H, W, 3] containing the rendered
+				image.
+		"""
+		im = 0.33 * im_real.astype(np.float32) + 0.67 * im_render.astype(np.float32)
+		im = im.astype(np.uint8)
+		return im
+
+	def _render_color(self):
+		"""Renders and saves mesh images."""
+		for d in self._render_dir:
+			os.makedirs(d, exist_ok=True)
+
+		if self._verbose:
+			print('Rendering mesh image')
+		for c in range(self._num_cameras):
+
+			video_writer = skvideo.io.FFmpegWriter(self._render_dir[c] + '/traj.mp4', 
+				inputdict={'-r': str(self._framerate), '-s':'{}x{}'.format(self._w, self._h)},
+				outputdict={'-c:v': 'libx264', '-r': str(self._framerate), '-preset': 'ultrafast',})
+
+			print("Write video frame...")
+			for i in tqdm(range(self._num_frames)):
+				if self._verbose:
+					print('{:03d}/{:03d}'.format(i + 1, self._num_frames))
+
+				mano_vert = [[y[i] for y in x] for x in self._mano_vert]
+
+				# Create pyrender scene.
+				scene = pyrender.Scene(bg_color=np.array([0.0, 0.0, 0.0, 0.0]),
+									  ambient_light=np.array([1.0, 1.0, 1.0]))
+
+				# Add camera.
+				cam_pose = np.eye(4)
+				scene.add(self._cameras[c], pose=cam_pose)
+
+				vert_m = mano_vert[c]
+
+				# Add MANO meshes.
+				for o in range(self._num_mano):
+					if np.all(vert_m[o] == 0.0):
+						continue
+					vert = vert_m[o].copy()
+					vert[:, 1] *= -1
+					vert[:, 2] *= -1
+					mesh = trimesh.Trimesh(vertices=vert, faces=self._faces[i])
+					mesh1 = pyrender.Mesh.from_trimesh(mesh)
+					mesh1.primitives[0].material.baseColorFactor = [0.7, 0.7, 0.7, 1.0]
+					mesh2 = pyrender.Mesh.from_trimesh(mesh, wireframe=True)
+					mesh2.primitives[0].material.baseColorFactor = [0.0, 0.0, 0.0, 1.0]
+					node1 = scene.add(mesh1)
+					node2 = scene.add(mesh2)
+
+				color, _ = self._r.render(scene)
+
+				if self._images is not None:
+					im = self._images[c][i]
+					b_color = self._blend(im, color)
+				else:
+					b_color = color
+
+				if not self._video_only:
+					color_im_file = self._render_dir[c] + "/color_{:06d}.jpg".format(i)
+					cv2.imwrite(color_im_file, b_color[:, :, ::-1])
+				# video_writer.write(b_color[:, :, ::-1])
+				video_writer.writeFrame(b_color[:, :, ::-1])
+
+			#video_writer.release()
+			video_writer.close()
+
+	def run(self):
+		"""Runs the renderer."""
+		self._render_color()
+		# self._render_joint()
+	
+	def close(self):
+		"""Closes the renderer."""
+		self._r.delete()
